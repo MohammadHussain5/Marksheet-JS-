ad95bdd3fa476cd2166a3d09563e49021deabf56
Mohammad Hussain <165736304+MohammadHussain5@users.noreply.github.com>
2024-05-01 02:21:51 -0700

    Add files via upload

---
+++ ms.js
@@ -0,0 +1,26 @@
+let result = prompt("enter your number");
+
+if (result >= 90) {
+
+    console.log("A+ Grade");
+
+}
+
+else if (result >= 80) {
+    console.log("A Grade");
+}
+
+else if (result >= 70) {
+    console.log("B+ Grade");
+}
+else if (result >=60){
+    console.log("B Grade");
+}
+
+else if (result >= 50) {
+    console.log("C Grade");
+}
+else if (result <50){
+    console.log("Fail");
+}
+
